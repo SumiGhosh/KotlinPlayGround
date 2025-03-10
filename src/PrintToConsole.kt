ff61f9a2d34ec49f40e7de7a7015c12a53787139
sumighosh17 <sumi.ghosh@nuware.com>
2025-03-10 18:31:49 +0530

    Collection List, Array, Set, Map, Sequence. General Operations. Possible Questions. Source: Medium and Gemini.

---
+++ src/PrintToConsole.kt
@@ -0,0 +1,9 @@
+object PrintToConsole {
+    fun println(data:Any) {
+        System.out.println("Print statement $data")
+    }
+
+    fun print(data:Any) {
+        System.out.print("$data")
+    }
+}
